b58f132ae1ddaf95799903ea3f5ca0a266e67de3
SchooiCodes <118210923+SchooiCodes@users.noreply.github.com>
2024-05-20 15:10:50 +0300

    Update SchooiMultitool.bat

--- SchooiMultitool.bat
+++ SchooiMultitool.bat
@@ -1,22 +1,3 @@
-@ECHO OFF
-REM  QBFC Project Options Begin
-REM  HasVersionInfo: No
-REM Companyname: 
-REM Productname: 
-REM Filedescription: 
-REM Copyrights: 
-REM Trademarks: 
-REM Originalname: 
-REM Comments: 
-REM Productversion:  0. 0. 0. 0
-REM Fileversion:  0. 0. 0. 0
-REM Internalname: 
-REM ExeType: console
-REM Architecture: x64
-REM Appicon: 
-REM AdministratorManifest: No
-REM  QBFC Project Options End
-@ECHO ON
 @echo off
 REM Anti-Piracy
 if "%~dp0"=="C:\Windows\System32" call :elevate
