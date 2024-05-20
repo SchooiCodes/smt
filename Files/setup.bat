55025341bd31e746fb5ccefb5297a71749b6ae3b
SchooiCodes <118210923+SchooiCodes@users.noreply.github.com>
2024-05-20 16:13:49 +0300

    Update setup.bat

--- Files/setup.bat
+++ Files/setup.bat
@@ -1,11 +1,5 @@
 @echo off
 title SMT ^| Setup
-fltmc >nul 2>&1 || (
-    PowerShell Start -Verb RunAs '%0' 2> nul || (
-        >nul pause && exit 1
-    )
-    exit 0
-)
 echo Disclaimer:
 echo This Multitool (the "Schooi's Multitool") is provided for ethical purposes only. The creators and contributors of this tool do not condone, support, or endorse any illegal or unethical activities. The use of this tool for unauthorized access to computer systems, networks, or data is strictly prohibited and may violate local, national, and international laws.
 echo By accessing, downloading, or using this tool, you agree to use it responsibly and lawfully. You acknowledge that any misuse of this tool, including but not limited to unauthorized access, data theft, or disruption of services, may result in legal consequences and civil liability.
@@ -14,21 +8,29 @@ echo This tool is provided "as is" without any warranty of any kind, express or 
 echo By using this tool, you agree to indemnify and hold harmless the creators and contributors from any claims, damages, losses, liabilities, or expenses arising out of or in connection with your use of the tool.
 echo Type "I AGREE" to continue.
 set /p agree=^> 
-if not "%agree%"=="I AGREE" exit
-if NOT "%CD:~0,3%"=="C:\" goto USB
-if "%CD:~0,3%"=="C:\" goto C
+if /I not "%agree%"=="I AGREE" exit:
+if /I "%CD:~0,1%"=="C" goto C
+goto USB
 
 :C
-cd C:\Program Files
-md "Schooi's Multitool"
+fltmc >nul 2>&1 || (
+    PowerShell Start -Verb RunAs '%0' 2> nul || (
+        >nul pause && exit 1
+    )
+    exit 0
+)
+cd /d "C:\Program Files"
+md "Schooi's Multitool" 2>nul
 cd "Schooi's Multitool"
 copy /y NUL needed_file.schm >NUL
 attrib +h +s needed_file.schm
 del "%~f0"
+pause
 exit
 
 :USB
-cd %CD:~0,3%
-copy /y NUL needed_file.schm >NUL
+if NOT "%cd%"=="%CD:~0,3%" cd .. & goto USB
+copy /y NUL needed_file.schm >NUL 
 attrib +h +s needed_file.schm
 del "%~f0"
+exit
