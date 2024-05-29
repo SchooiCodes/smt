0fafac56483bb838bdf50e7d7f6dd5b06e1bfc5c
Schooi <118210923+SchooiCodes@users.noreply.github.com>
2024-05-29 16:26:55 +0300

    Delete Files directory

--- Files/mystery.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title THE BEST TOOL
-echo Initializing...
-if not exist music.mp3 powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://schooicodes.github.io/file_hosting/music.mp3', '%~dp0\music.mp3')
-echo Done.
-title Get rickrolled!
-start /MIN music.bat
-curl ascii.live/rick
