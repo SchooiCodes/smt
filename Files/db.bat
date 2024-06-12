b71c17c76785781a2925d5d84cbc6861a1e86bac
Schooi <118210923+SchooiCodes@users.noreply.github.com>
2024-06-12 21:14:46 +0300

    v2.3: What was I on when I made this??

--- Files/db.bat
+++ Files/db.bat
@@ -5,11 +5,17 @@ fltmc >nul 2>&1 || (
     )
     exit 0
 )
-title Drivers Backup ^| v1.0
-echo Hello, and welcome to Drivers Backup, another tool created by - guess who - SCHOOI! Let's get started
-echo Please enter the export location (the place where the backups will be stored. We recommend you give this script an empty folder)
+title Driver Backupper ^| v1.0
+cd %~dp0
+call logo.bat
+echo.
+echo Driver Backupper
+echo =============
+echo Please enter the export location (the place where the backups will be stored. I recommend you give this script an empty folder)
 echo It has to be a full path, eg. C:\Windows\system32 or D:\Drivers Backup
 set /p loc=^>
 dism /online /export-driver /destination:"%loc%"
+if %ERRORLEVEL% EQU 0 echo Success!
+if NOT %ERRORLEVEL% EQU 0 echo Error: %ERRORLEVEL%
 pause >nul
 exit
