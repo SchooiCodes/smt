04022e7582aaac59de66f4f0ccc2cb1d140c894a
Schooi <118210923+SchooiCodes@users.noreply.github.com>
2024-07-01 14:06:02 +0300

    Update IB.bat

--- Files/IB.bat
+++ Files/IB.bat
@@ -1,12 +1,16 @@
 @echo off
-
+title Registry Backup Importer
+call logo.bat
+echo.
+echo Registry Backup Importer
+echo ========================
 echo This script will import the registry backups.
 echo Make sure you've backed up your important data and system before proceeding.
 echo Press any key to continue, or Ctrl+C to exit.
 pause >nul
 
 :1
-echo Where are the backups located? (Full path required, eg C:\Users\user\Desktop)
+echo Where are the backups located? (Full path required, eg C:\Users\%USERNAME%\Desktop)
 set /p loc=^> 
 cd "%loc%"
 if ERRORLEVEL 1  echo Invalid location! & goto 1
@@ -19,6 +23,7 @@ reg import HKLM_backup.reg
 reg import HKU_backup.reg
 reg import HKCC_backup.reg
 
-echo Registry backups imported successfully.
+if %ERRORLEVEL% EQU 0 echo Registry backups imported successfully.
+if NOT %ERRORLEVEL% EQU 0 echo Registry backups imported unsuccessfully, errorlevel: %ERRORLEVEL%.
 
 pause
