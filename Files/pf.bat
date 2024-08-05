d541b32c78d9d29bf2ec213bb4ca31a1dd0a9aab
Schooi <robloxianskp@gmail.com>
2024-08-05 11:18:28 +0300

    bug lol

--- Files/pf.bat
+++ Files/pf.bat
@@ -22,7 +22,7 @@ set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%USERPROFILE%\Desktop\Schooi's Multitool.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "C:\Program Files\SchooiMultitool\SchooiMultitool.bat" >> %SCRIPT%
+echo oLink.TargetPath = "C:\Program Files\Schooi's Multitool\SchooiMultitool.bat" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 cscript /nologo %SCRIPT%
 del %SCRIPT%
