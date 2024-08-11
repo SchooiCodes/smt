1b5e4d3f3c1a58a08effdfb135fcc969b1c1a7bf
Schooi <robloxianskp@gmail.com>
2024-08-11 21:15:11 +0300

    Offline Restore Points

--- Files/autorespo.bat
+++ Files/autorespo.bat
@@ -1,8 +1,6 @@
 @echo off
 cd %~dp0
 title Restore Point Creator
-
-:: Ensure the script is running with administrative privileges
 fltmc >nul 2>&1 || (
 	echo This script is not elevated!
 	echo Requesting Admin permissions..
@@ -11,23 +9,12 @@ fltmc >nul 2>&1 || (
     )
     exit
 )
-
-
-:: Call logo.bat, ensure the path is correct
-if exist "%~dp0logo.bat" (
-    call "%~dp0logo.bat"
-) else (
-    echo logo.bat not found in %~dp0
-    pause
-    exit 1
-)
-
+call logo.bat
 echo.
 echo Creating restore point...
-
-:: Download and execute the PowerShell script
-powershell -Command "iwr -useb https://schooicodes.github.io/file_hosting/autorespo.ps1 | iex"
-
+REM powershell -Command "iwr -useb https://schooicodes.github.io/file_hosting/autorespo.ps1 | iex"
+if not exist autorespo.ps1 type ar.txt>>autorespo.ps1
+powershell -Command "iex .\autorespo.ps1"
 :: Uncomment these lines if you want to use wmic for restore point creation
 ::wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Automatic Restore Point (Schooi's Multitool)", 100, 7
 ::if %errorlevel% EQU 0 (
@@ -37,5 +24,6 @@ powershell -Command "iwr -useb https://schooicodes.github.io/file_hosting/autore
 ::) else (
 ::    echo Error! Errorlevel: %errorlevel%
 ::)
-
-pause
+echo Press any key to exit.
+pause >nul
+exit
