4243e2ec42ab3a2ac0edd102e222b6c2090db588
Schooi <robloxianskp@gmail.com>
2024-08-15 23:26:01 +0300

    Updated URL Shortener

--- Files/URLShortener.bat
+++ Files/URLShortener.bat
@@ -1,10 +1,13 @@
-@echo off
+@echo on
 title URL Shortener by Schooi
 call logo.bat
 echo.
 echo URL SHORTENER
 echo ---------------------
 set /p url= Enter the URL to shorten: 
-echo Success!
+echo %url% | findstr "https://" >nul
+if ERRORLEVEL 1 set url=https://%url%
 curl https://tinyurl.com/api-create.php?url=%url%
+if ERRORLEVEL 0 echo Success!
 pause >nul
+exit
