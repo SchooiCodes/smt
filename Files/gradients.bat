d57e330b21e385865fcd0289d4c4b11453130138
Schooi <robloxianskp@gmail.com>
2024-08-17 23:00:51 +0300

    New Gradients

--- Files/gradients.bat
+++ Files/gradients.bat
@@ -11,12 +11,15 @@ echo ONLY 6 LINE ASCII ART IS SUPPORTED!
 echo TO GENERATE 6 LINE ASCII ART: https://bit.ly/6linetallascii
 echo Which color would you like it to be?
 echo [1] [38;5;214mOrange[97m --^> [38;5;219mPink[97m, [2] [38;5;40mGreen[97m --^> [38;5;45mBright Blue[97m, [3] [38;5;33mBlue[97m --^> [38;5;213mPink[97m [4] [38;5;196mRed[97m --^> [38;5;201mPink[97m [5] [38;5;46mGreen[97m --^> [38;5;226mYellow[97m
+echo [6] [38;5;51mBlue[97m -^> [38;5;226mYellow[97m, [38;2;135;206;250mLight Blue[97m -^> [38;2;35;106;200mBlue[97m
 set /p choice=^> 
 if %choice%==1 goto otp
 if %choice%==2 goto gtbb
 if %choice%==3 goto dbtp
 if %choice%==4 goto rtp
 if %choice%==5 goto gty
+if %choice%==6 goto bty
+if %choice%==7 goto bbtb
 goto start
 
 
@@ -128,6 +131,50 @@ set "l5=echo ^<escape^>[38;5;190m%l5%"
 echo Please paste the sixth line of your ASCII art: 
 set /p l6=^> 
 set "l6=echo ^<escape^>[38;5;226m%l6%^<escape^>[0m"
+call :scags
+
+:bty
+echo.
+echo Please paste the first line of your ASCII art: 
+set /p l1=^> 
+set "l1=echo ^<escape^>[38;5;51m%l1%"
+echo Please paste the second line of your ASCII art: 
+set /p l2=^> 
+set "l2=echo ^<escape^>[38;5;86m%l2%"
+echo Please paste the third line of your ASCII art: 
+set /p l3=^> 
+set "l3=echo ^<escape^>[38;5;121m%l3%"
+echo Please paste the fourth line of your ASCII art: 
+set /p l4=^> 
+set "l4=echo ^<escape^>[38;5;156m%l4%"
+echo Please paste the fifth line of your ASCII art: 
+set /p l5=^> 
+set "l5=echo ^<escape^>[38;5;191m%l5%"
+echo Please paste the sixth line of your ASCII art: 
+set /p l6=^> 
+set "l6=echo ^<escape^>[38;5;226m%l6%^<escape^>[0m"
+call :scags
+
+:bbtb
+echo.
+echo Please paste the first line of your ASCII art: 
+set /p l1=^> 
+set "l1=echo ^<escape^>[38;2;135;206;250m%l1%"
+echo Please paste the second line of your ASCII art: 
+set /p l2=^> 
+set "l2=echo ^<escape^>[38;2;115;186;240m%l2%"
+echo Please paste the third line of your ASCII art: 
+set /p l3=^> 
+set "l3=echo ^<escape^>[38;2;95;166;230m%l3%"
+echo Please paste the fourth line of your ASCII art: 
+set /p l4=^> 
+set "l4=echo ^<escape^>[38;2;75;146;220m%l4%"
+echo Please paste the fifth line of your ASCII art: 
+set /p l5=^> 
+set "l5=echo ^<escape^>[38;2;55;126;210m%l5%"
+echo Please paste the sixth line of your ASCII art: 
+set /p l6=^> 
+set "l6=echo ^<escape^>[38;2;35;106;200m%l6%^<escape^>[0m"
 call :scags
 
 :scags
