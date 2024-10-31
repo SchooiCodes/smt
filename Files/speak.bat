f4f4aef13d6e04e8d53946568ca14aa5f0fef270
SchooiCodes <robloxianskp@gmail.com>
2024-10-31 17:23:59 +0200

    Updated the speak.vbs file to be downloaded instead to null out all vbs on this repo and made the logo.bat file in apps match the one in .."

---
+++ Files/speak.bat
@@ -0,0 +1,12 @@
+@echo off
+call logo.bat
+echo.
+echo Generating needed file..
+echo Dim message, sapi > speak.vbs
+echo message=InputBox("What do you want me to say?","Speak to Me") >> speak.vbs
+echo Set sapi=CreateObject("sapi.spvoice") >> speak.vbs
+echo sapi.Speak message >> speak.vbs
+start /WAIT speak.vbs
+del speak.vbs
+pause
+exit
--- Files/speak.vbs
+++
@@ -1,4 +0,0 @@
-Dim message, sapi
-message=InputBox("What do you want me to say?","Speak to Me")
-Set sapi=CreateObject("sapi.spvoice")
-sapi.Speak message
