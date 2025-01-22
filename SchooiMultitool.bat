5b25ce80a058e2eff2afd5c8721a01cd1d58b00a
SchooiCodes <robloxianskp@gmail.com>
2025-01-22 15:35:39 +0200

    Added npp.bat to SchooiMultitool.bat

--- SchooiMultitool.bat
+++ SchooiMultitool.bat
@@ -328,6 +328,7 @@ echo 16. Firefox
 echo 17. 7zip
 echo 18. Telegram
 echo 19. Malwarebytes
+echo 20. Notepad++
 echo.
 set /p appch=%BRIGHT_GREEN%%username%@smt%RESET%:%BRIGHT_BLUE%~%BRIGHT_WHITE%$ 
 if /i "%appch%"=="B" cls & goto AdvancedTools
@@ -350,6 +351,7 @@ if "%appch%"=="16" start Apps\firefox.bat
 if "%appch%"=="17" start Apps\7z.bat
 if "%appch%"=="18" start Apps\telegram.bat
 if "%appch%"=="19" start Apps\mbam.bat
+if "%appch%"=="20" start Apps\npp.bat
 goto apps
 
 :danger
