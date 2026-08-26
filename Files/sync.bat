d99376c18f6707c36c7df12fac551da1b7ec04c8
SchooiCodes <robloxianskp@gmail.com>
2026-08-26 13:23:41 +0300

    updated to avoid loopsyncing in portable installs
    yes

--- Files/sync.bat
+++ Files/sync.bat
@@ -18,6 +18,4 @@ if exist logo.bat call logo.bat & echo.
 echo Running sync..
 cd ..
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $ProgressPreference = 'SilentlyContinue'; irm https://raw.githubusercontent.com/SchooiCodes/file_hosting/refs/heads/main/sync.ps1 -OutFile sync.ps1"
-powershell ./sync.ps1 
-del sync.ps1
-exit
+powershell ./sync.ps1 & del sync.ps1 & start "" "SchooiMultitool.bat" %* updated & exit
